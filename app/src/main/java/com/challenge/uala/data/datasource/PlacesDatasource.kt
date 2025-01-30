2efb72778087df9f3451cfb0bb9f6d4f9c356c3d
Servando Reyes <l1007650@bancogalicia.com.ar>
2025-01-30 19:03:41 -0300

    A datasource class was created in order to read the json file from resources.

---
+++ app/src/main/java/com/challenge/uala/data/datasource/PlacesDatasource.kt
@@ -0,0 +1,20 @@
+package com.challenge.uala.data.datasource
+
+import android.content.res.Resources
+import androidx.annotation.RawRes
+import javax.inject.Inject
+
+class PlacesDatasource @Inject constructor(private val resources: Resources) {
+    suspend fun readJsonFromRaw(@RawRes resourceId: Int): String? {
+        return try {
+            val inputStream = resources.openRawResource(resourceId)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer)
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
