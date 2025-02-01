0ec372db8dfef928875039e56aae0833cde130a5
Servando Reyes <l1007650@bancogalicia.com.ar>
2025-01-31 21:29:42 -0300

    maps compose gradle dependency was added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-compiler:2.52")
 
+    // Maps
+    implementation("com.google.maps.android:maps-compose:6.4.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
